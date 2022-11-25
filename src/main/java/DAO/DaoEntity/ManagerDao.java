fd7d9029dfa6039883ef9102ce759c27e7450781
abdessalm staili <abdoycode@gmail.com>
2022-11-25 18:59:50 +0100

    pull the last version

--- src/main/java/DAO/DaoEntity/ManagerDao.java
+++ src/main/java/DAO/DaoEntity/ManagerDao.java
@@ -3,8 +3,11 @@ package DAO.DaoEntity;
 import DAO.AbstractHibernateDAO;
 import Entity.Chauffeur;
 import Entity.Manager;
+import jakarta.persistence.NoResultException;
 
 import java.util.List;
+
+import static Utils.Hash.verifiedPassword;
 
 public class ManagerDao extends AbstractHibernateDAO<Manager> {
     public ManagerDao() {
@@ -28,5 +31,36 @@ public class ManagerDao extends AbstractHibernateDAO<Manager> {
     //delete Manager
     public void deleteManagerByID(long id){
         deleteById(id);
+    }
+    // find one manager by email
+    public static Manager getManagerByEmail(String email) {
+
+        return jpaService.runInTransaction(entityManager -> {
+            try{
+                return entityManager.createQuery("select a from Manager a WHERE a.email = :email", Manager.class)
+                        .setParameter("email", email)
+                        .getSingleResult();
+            } catch (NoResultException nre){
+                return null;
+            }
+        });
+
+    }
+
+    //validate login
+    public static boolean validateAdminLogin(Object[] login){
+        String email = (String) login[0];
+        String password = (String) login[1];
+        Manager manager = getManagerByEmail(email);
+        assert manager != null;
+        if(manager != null) {
+            if (password.equals(manager.getPassword())) {
+                return true;
+            } else {
+                return false;
+            }
+        }else {
+            return false;
+        }
     }
 }
